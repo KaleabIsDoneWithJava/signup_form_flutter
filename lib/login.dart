e3936030613981902502bbbe4759bd3a59742ad1
KaleabIsDoneWithJava <kaleab.teshome16@gmail.com>
2023-12-06 09:57:22 +0300

    Implemented login & signup interfaces

---
+++ lib/login.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+/// A Flutter widget representing the login page.
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          margin: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _header(context),
+              _inputField(context),
+              _forgotPassword(context),
+              _signup(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// Widget for displaying the header text.
+  Widget _header(context) {
+    return const Column(
+      children: [
+        Text(
+          "Welcome Back",
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+        Text("Enter your credential to login"),
+      ],
+    );
+  }
+
+  /// Widget for displaying the input fields (username and password).
+  Widget _inputField(context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          decoration: InputDecoration(
+              hintText: "Username",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(18),
+                  borderSide: BorderSide.none),
+              fillColor: Colors.purple.withOpacity(0.1),
+              filled: true,
+              prefixIcon: const Icon(Icons.person)),
+        ),
+        const SizedBox(height: 10),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Password",
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18),
+                borderSide: BorderSide.none),
+            fillColor: Colors.purple.withOpacity(0.1),
+            filled: true,
+            prefixIcon: const Icon(Icons.password),
+          ),
+          obscureText: true,
+        ),
+        const SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: () {
+            // TODO: Implement login functionality
+          },
+          style: ElevatedButton.styleFrom(
+            shape: const StadiumBorder(),
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            backgroundColor: Colors.purple,
+          ),
+          child: const Text(
+            "Login",
+            style: TextStyle(fontSize: 20),
+          ),
+        )
+      ],
+    );
+  }
+
+  /// Widget for displaying the "Forgot password?" link.
+  Widget _forgotPassword(context) {
+    return TextButton(
+      onPressed: () {
+        // TODO: Implement forgot password functionality
+      },
+      child: const Text(
+        "Forgot password?",
+        style: TextStyle(color: Colors.purple),
+      ),
+    );
+  }
+
+  /// Widget for displaying the "Sign Up" link.
+  Widget _signup(context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have an account? "),
+        TextButton(
+            onPressed: () {
+              // TODO: Implement sign up functionality
+            },
+            child: const Text(
+              "Sign Up",
+              style: TextStyle(color: Colors.purple),
+            ))
+      ],
+    );
+  }
+}
